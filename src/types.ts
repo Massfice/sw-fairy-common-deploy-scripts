e7ae5047ae367d41b8d111a31a1661b931f81270
Massfice <adrian.marian.tomasz.larysz@gmail.com>
2021-06-26 22:41:49 +0200

    Changes

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
 import { TaskFunction, TaskFunctionWrapped } from 'undertaker';
 
+interface KubernetesDeployConfig {
+    commitId: string;
+}
+
 type KubernetessDeployFunction = (config: null) => TaskFunction | TaskFunctionWrapped;
 
 type PackageDeployFunction = (config: null) => TaskFunction | TaskFunctionWrapped;
 
-export { KubernetessDeployFunction, PackageDeployFunction };
+export { KubernetessDeployFunction, PackageDeployFunction, KubernetesDeployConfig };
